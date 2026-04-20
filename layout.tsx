fdb6c82e1ff940b10e93c6f33c91908be657941a
kobichen222 <kobi@media-deal.co.il>
2026-04-20 05:13:21 +0300

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export default function DJPage() {
+  return (
+    <iframe
+      src="/pioneer-dj-pro-max-v2.html"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+        position: "fixed",
+        top: 0,
+        left: 0,
+      }}
+      title="Pioneer DJ Pro MAX"
+    />
+  );
+}
